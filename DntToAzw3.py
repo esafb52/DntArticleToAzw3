1d2bbe72391e4703efa0e5214c4c50dc10868958
masiha <esafb52@gmail.com>
2021-01-07 21:46:00 +0330

    clean code

--- DntToAzw3.py
+++ DntToAzw3.py
@@ -5,10 +5,13 @@ from bs4 import BeautifulSoup
 
 
 def get_calibre_path():
-    res = platform.architecture()
-    if '32bit' in res[0]:
-        return 'C:/PROGRA~2/Calibre2'
-    return 'C:/PROGRA~1/Calibre2'
+    res = platform.architecture()[0]
+    calibre_path = 'C:/PROGRA~2/Calibre2'
+    if '32bit' not in res:
+        calibre_path = 'C:/PROGRA~1/Calibre2'
+    if os.path.exists(calibre_path):
+        return calibre_path
+    return None
 
 
 def get_course_files(soup_content):
@@ -51,17 +54,15 @@ def end_body():
 def write_book(courses, title, article_dir, course_out_dir):
     start_body_section = "class='main'"
     comment_section = '<h3>نظرات</h3>'
-    course_file_name = '{0}.html'.format(title).replace('  ', '')
+    course_file_name = '{0}.html'.format(title)
     write_mode = False
-
-    course_file = open(os.path.join(course_out_dir, course_file_name), 'w', encoding='utf-8')
+    new_course = os.path.join(course_out_dir, course_file_name)
+    course_file = open(new_course, 'w', encoding='utf-8')
     course_file.write(body_section(title))
-
     for course in courses:
-        this_book = os.path.join(article_dir, course)
         if not course.endswith('.html') or title is None:
             continue
-        with open(this_book, 'r', encoding='utf-8') as book_content:
+        with open(os.path.join(article_dir, course), 'r', encoding='utf-8') as book_content:
             for line in book_content.readlines():
                 this_line = str(line).strip()
                 if comment_section in this_line:
@@ -72,13 +73,12 @@ def write_book(courses, title, article_dir, course_out_dir):
                     write_mode = True
                 if write_mode:
                     course_file.write(this_line + '\n')
-
     course_file.write(end_body())
     course_file.close()
-    print('task  {0} complete'.format(title), '=' * 40)
+    print('task  {0} complete'.format(title))
 
 
-def get_articles_content(articles_dir, out_book_dir):
+def convert_articles_content(articles_dir, out_book_dir):
     if not os.path.exists(out_book_dir):
         os.mkdir(out_book_dir)
     course_info = os.path.join(articles_dir, 'path.html')
@@ -86,29 +86,37 @@ def get_articles_content(articles_dir, out_book_dir):
         content = course_info.read().split('<h2>')
         titles = get_course_titles(articles_dir)
         for part in content:
-            soup = BeautifulSoup(part, 'html.parser')
-            course_files = get_course_files(soup)
-            course_title = titles.pop(0)
-            write_book(course_files, course_title, articles_dir, out_book_dir)
+            try:
+                soup = BeautifulSoup(part, 'html.parser')
+                course_files = get_course_files(soup)
+                course_title = titles.pop(0)
+                write_book(course_files, course_title, articles_dir, out_book_dir)
+            except Exception as e:
+                print(e, 'error!!!')
 
 
 def convert_to_azw3(articles_dir):
-    out_files = os.listdir(articles_dir)
-    for file in out_files:
-        if '.html' in file:
-            file_path = os.path.join(articles_dir, file)
-            out_file_path = os.path.join(articles_dir, file[:-4] + 'azw3')
-            if not os.path.exists(out_file_path):
-                try:
-                    cmd = '{0}/ebook-convert.exe {1} {2}'.format(get_calibre_path(), file_path, out_file_path)
-                    os.system(cmd)
-                except Exception as e:
-                    print(e, 'error!!!!')
+    if get_calibre_path() is not None:
+        html_book_files = os.listdir(articles_dir)
+        for file in html_book_files:
+            if file.endswith('.html'):
+                html_book_file = os.path.join(articles_dir, file)
+                azw3_file = os.path.join(articles_dir, file[:-4] + 'azw3')
+                if not os.path.exists(azw3_file):
+                    try:
+                        cmd = '{0}/ebook-convert.exe {1} {2}'.format(get_calibre_path(), html_book_file, azw3_file)
+                        os.system(cmd)
+                    except Exception as e:
+                        print(e, 'error!!!!')
+    else:
+        print('calibre is not install on this pc !!!')
 
 
 if __name__ == '__main__':
+    print('=' * 10, 'start !!!', '=' * 10)
     articles = 'C:/Users/masiha/Desktop/dnt-1399-10-16/OPF/articles'
-    book_out_dir = 'C:/Users/masiha/Desktop/dnt-1399-10-16/final_book_farsi_final22323'
-    get_articles_content(articles, book_out_dir)
-    convert_to_azw3(book_out_dir)
-    print('complete all tasks!')
+    save_as_book = 'C:/Users/masiha/Desktop/dnt-1399-10-16/final_book_farsi_final_mm'
+    convert_articles_content(articles, save_as_book)
+    convert_to_azw3(save_as_book)
+    print('=' * 10, 'complete all tasks  !!!', '=' * 10)
+
